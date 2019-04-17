f376d5cac2f145e624a73490ac8cd8780f1b5063
Paulo Santos <paulohcsantos_@outlook.com>
2019-04-17 10:35:29 -0300

    first commit

---
+++ CompressPDF/CompressPdf.cs
@@ -0,0 +1,81 @@
+﻿using Ghostscript.NET;
+using Ghostscript.NET.Processor;
+using System.Collections.Generic;
+using System;
+
+namespace CompactarPdf
+{
+    public class CompressPdf
+    {
+        private GhostscriptVersionInfo _gs_verssion_info;
+        private string _inputFile;
+        private string _outputFile;
+
+        public string LIB
+        {
+            get
+            {
+                if (Environment.Is64BitOperatingSystem)
+                    return Environment.CurrentDirectory + "\\gs\\gs9.22-64\\bin;" +
+                           Environment.CurrentDirectory + "\\gs\\gs9.22-64\\lib;" +
+                           Environment.CurrentDirectory + "\\gs\\gs9.22-64\\fonts";
+                else
+                    return Environment.CurrentDirectory + "\\gs\\gs9.22-32\\bin;" +
+                           Environment.CurrentDirectory + "\\gs\\gs9.22-32\\lib;" +
+                           Environment.CurrentDirectory + "\\gs\\gs9.22-32\\fonts";
+            }
+        }
+
+        public string DLL
+        {
+            get
+            {
+                if (Environment.Is64BitOperatingSystem)
+                    return Environment.CurrentDirectory + "\\gs\\gs9.22-64\\bin\\gsdll64.dll";
+                else
+                    return Environment.CurrentDirectory + "\\gs\\gs9.22-32\\bin\\gsdll32.dll";
+            }
+        }
+        
+        public CompressPdf(string nameFile, string newNameFile)
+        {
+            this._inputFile = nameFile;
+            this._outputFile = newNameFile;
+            this._gs_verssion_info = new GhostscriptVersionInfo(new System.Version("9.22"), DLL, LIB, GhostscriptLicense.GPL);
+        }
+
+        public void ProcessFiles()
+        {
+            CompressDocument();
+        }
+
+        private void CompressDocument()
+        {
+            List<string> gsArgs = new List<string>();
+
+            gsArgs.Add("-empty");
+            gsArgs.Add("-dSAFER");
+            gsArgs.Add("-dBATCH");
+            gsArgs.Add("-dNOPAUSE");
+            gsArgs.Add("-dNOPROMPT");
+
+            gsArgs.Add("-sDEVICE=pdfwrite");
+            gsArgs.Add("-dCompatibilityLevel=1.4");
+            // dPDFSETTINGS is basically our commpression option
+            //  - /screen selects low-resolution output similar to the Acrobat Distiller "Screen Optimized" setting.
+            //  - /ebook selects medium-resolution output similar to the Acrobat Distiller "eBook" setting.
+            //  - /printer selects output similar to the Acrobat Distiller "Print Optimized" setting.
+            //  - /prepress selects output similar to Acrobat Distiller "Prepress Optimized" setting.
+            //  - /default selects output intended to be useful across a wide variety of uses, possibly at the expense of a larger output file.
+            // gsArgs.Add("-dPDFSETTINGS=/screen");
+            gsArgs.Add("-dPDFSETTINGS=/ebook");
+
+            gsArgs.Add("-sOutputFile=" + _outputFile + "");
+            gsArgs.Add("-f");
+            gsArgs.Add(_inputFile);
+
+            using (GhostscriptProcessor processor = new GhostscriptProcessor(_gs_verssion_info, true))
+                processor.StartProcessing(gsArgs.ToArray(), null);
+        }
+    }
+}
---
+++ CompressPDF/Program.cs
@@ -0,0 +1,65 @@
+﻿using NDesk.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CompactarPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arquivo_original = "";
+            string arquivo_otimizado = "";
+
+            var p = new OptionSet() {
+                { "i|input=", "caminho e nome do arquivo a ser otimizado (Obrigatório)",
+                    v => arquivo_original = v },
+                { "o|output=", "caminho e nome do novo arquivo otimizado (Obrigatório)",
+                    v => arquivo_otimizado = v }
+            };
+
+            List<string> parametros;
+            try
+            {
+                parametros = p.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("OtimizaPDF: ");
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            try
+            {
+                if ((string.IsNullOrWhiteSpace(arquivo_original)) ||
+                    (string.IsNullOrWhiteSpace(arquivo_otimizado)))
+                    throw new Exception("Faltando parâmetros obrigatórios!");
+            }
+            catch (Exception e)
+            {
+                Console.Write("OtimizaPDF: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Tente `OtimizaPDF --help' para mais informações.");
+                return;
+            }
+
+            try
+            {
+                if (!File.Exists(arquivo_original.Replace('\"', ' ').Trim()))
+                    return;
+                var compress = new CompressPdf(arquivo_original.Replace('\"', ' ').Trim(), arquivo_otimizado.Replace('\"', ' ').Trim());
+                compress.ProcessFiles();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Houve uma falha na execução.");
+                Console.WriteLine("Verifique o erro:");
+                Console.WriteLine(e.Message);
+                return;
+            }
+        }
+
+    }
+}
